f7fa6a5b65a467a2f0d9fbccdf31550352a77754
MAC <ikadimi@student.1337.ma>
2020-10-24 14:44:45 +0100

    Updates

---
+++ precache-manifest.60e9386f429f952516681d9154afd4f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b6d4f911163082b87cc70953c8d258",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "818fe446ac04d1c0535f",
+    "url": "/portfolio/static/css/main.b54f02b1.chunk.css"
+  },
+  {
+    "revision": "abf673db9c388557afda",
+    "url": "/portfolio/static/js/2.64ba800a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.64ba800a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818fe446ac04d1c0535f",
+    "url": "/portfolio/static/js/main.2a70d3e5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
